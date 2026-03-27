5258e30efdcbd134e09d5560778c2022085052ed
tangge <tangge777@proton.me>
2026-03-27 14:03:26 +0800

    feat: init project

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,15 @@
-import { invoke } from '@tauri-apps/api/core'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useCallback } from 'react'
+import { toast } from 'sonner'
+import { Button } from './components/ui/button'
 
 export function App() {
-  const [greetMsg, setGreetMsg] = useState('')
-  const [name, setName] = useState('')
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke('greet', { name }))
-  }
+  const onClick = useCallback(() => {
+    toast.success('Hello world!')
+  }, [])
 
   return (
-    <main className="container">
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+    <main className="h-screen flex items-center justify-center">
+      <Button onClick={onClick}>Click me</Button>
     </main>
   )
 }
